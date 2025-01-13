33bae12b333b611e0ba9a88486561ca3dc0ed30e
Iurovet <s4003879@student.rmit.edu.au>
2025-01-13 16:37:14 +1100

    Add button to filp board.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import { useState } from 'react';
 
 function App() {
   const [orientation, setOrientation] = useState("White");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if (orientation === "White") {
+      setOrientation("Black");
+    }
+    if (orientation === "Black") {
+      setOrientation("White");
+    }
+  }
   
   return (
     <>
       {orientation === "White" && <WhiteRow/>}
       {orientation === "Black" && <BlackRow/>}
+
+      <form onSubmit={handleSubmit}>
+        <input type="submit" value="Flip board" />
+      </form>
     </>
   );
 }
